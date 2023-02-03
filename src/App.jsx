cca7170344bdaf627be65b28dcf1e1619339f480
Josue Albelaez <josuealbelaez@gmail.com>
2023-02-03 15:56:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Container, ModalBody } from 'react-bootstrap'
+import Formulario from './components/Formulario'
+import ListadoBebidas from './components/ListadoBebidas'
+import ModalBebida from './components/ModalBebida'
+import { CategoriasProvider } from './context/CategoriasProvider'
+import { BebidasProvider } from './context/BebidasProvider'
+import './App.css'
+
+function App() {
+
+  return (
+    <CategoriasProvider>
+      <BebidasProvider>
+          <header className="py-2">
+          <div >
+                <img className='logo' src="src/logo.png" alt="Logo take a drink" />
+                <p className='parrafo'>
+                 Los mejores tragos del mundo en un solo lugar
+                </p>
+            </div>
+            <h1>Busca tu trago</h1>
+          </header>
+
+
+          <Container className='mt-5'>
+              <Formulario />
+
+              <ListadoBebidas />
+
+              <ModalBebida />
+          </Container>
+      </BebidasProvider>
+    </CategoriasProvider>
+  )
+}
+
+export default App
